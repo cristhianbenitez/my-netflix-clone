4174004b071a754bd93c891d9c8b669e7f544c7f
Cristhian Benitez <cristhiantilleria@gmail.com>
2021-08-27 17:29:00 -0400

    netflix clone cristhian

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from './global-styles';
+import { App } from './app';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+  apiKey: 'AIzaSyD4-bcFZbgv-jo06suD80-vovlgqguyQxs',
+  authDomain: 'netflix-clone-5548d.firebaseapp.com',
+  databaseURL: 'https://netflix-clone-5548d-default-rtdb.firebaseio.com',
+  projectId: 'netflix-clone-5548d',
+  storageBucket: 'netflix-clone-5548d.appspot.com',
+  messagingSenderId: '1044699703093',
+  appId: '1:1044699703093:web:2d9f45d1165cda0a49ce7c',
+  measurementId: 'G-PGZXGBF4WX',
+};
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
